0965ec64050955d37c87d3b588f75fd836305c3e
mkanaparthy1 <103694453+mkanaparthy1@users.noreply.github.com>
2022-05-01 23:45:27 -0700

    Create SubtractTester.java

---
+++ SubtractTester.java
@@ -0,0 +1,9 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class SubtractTester {
+    @Test
+    public void subtraction(){
+        numbersSubtr(2, Subtraction.subtract(5,6));
+    }
+}
